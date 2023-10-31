d94d27efcd74a10e31c2b19f5504c84b819b0472
marcoslopez95 <50760667+marcoslopez95@users.noreply.github.com>
2023-10-31 10:34:50 -0400

    init

---
+++ database/migrations/2023_10_31_142832_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('left_logo')->nullable();
+            $table->string('right_logo')->nullable();
+            $table->string('header')->nullable();
+            $table->string('digital_firm')->nullable();
+            $table->string('parroco')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
